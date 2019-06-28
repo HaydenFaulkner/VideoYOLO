18cfcc861d97df7e47dfd35552b90ebcffdbfc22
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2019-06-28 15:10:07 +0930

    lets not get labels, and dont put images in images dir

--- datasets/get_coco_dataset.sh
+++ datasets/get_coco_dataset.sh
@@ -8,9 +8,6 @@ cd MSCoco
 git clone https://github.com/pdollar/coco
 mv coco cocoapi
 
-mkdir images
-cd images
-
 # Download and Unzip Images 2014 then 2017
 wget -c http://images.cocodataset.org/zips/train2014.zip
 wget -c http://images.cocodataset.org/zips/val2014.zip
@@ -28,7 +25,6 @@ unzip -q train2017.zip
 unzip -q val2017.zip
 unzip -q test2017.zip
 
-cd ..
 
 # Download COCO Metadata
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2014.zip
@@ -40,8 +36,8 @@ unzip -q annotations_trainval2017.zip
 unzip -q image_info_test2017.zip
 
 
-wget -c https://pjreddie.com/media/files/coco/labels.tgz
-tar xzf labels.tgz
+#wget -c https://pjreddie.com/media/files/coco/labels.tgz
+#tar xzf labels.tgz
 
 
 # Set Up Image Lists
