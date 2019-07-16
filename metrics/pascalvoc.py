5fbcf7ba40881ed18ba5f7648fc3baa6b771d07f
HaydenFaulkner <hay167167>
2019-07-17 01:45:15 +0930

    take out the as numpy

--- metrics/pascalvoc.py
+++ metrics/pascalvoc.py
@@ -6,6 +6,9 @@ import numpy as np
 import mxnet as mx
 from gluoncv.utils.bbox import bbox_iou
 
+from utils.general import as_numpy
+
+
 class VOCMApMetric(mx.metric.EvalMetric):
     """
     Calculate mean AP for object detection task
@@ -101,18 +104,6 @@ class VOCMApMetric(mx.metric.EvalMetric):
             Ground-truth bounding boxes difficulty labels with shape `B, M`.
 
         """
-        def as_numpy(a):
-            """Convert a (list of) mx.NDArray into numpy.ndarray"""
-            if isinstance(a, (list, tuple)):
-                out = [x.asnumpy() if isinstance(x, mx.nd.NDArray) else x for x in a]
-                try:
-                    out = np.concatenate(out, axis=0)
-                except ValueError:
-                    out = np.array(out)
-                return out
-            elif isinstance(a, mx.nd.NDArray):
-                a = a.asnumpy()
-            return a
 
         if gt_difficults is None:
             gt_difficults = [None for _ in as_numpy(gt_labels)]
@@ -120,7 +111,6 @@ class VOCMApMetric(mx.metric.EvalMetric):
         if isinstance(gt_labels, list):
             if len(gt_difficults) != len(gt_labels) * gt_labels[0].shape[0]:
                 gt_difficults = [None] * len(gt_labels) * gt_labels[0].shape[0]
-
 
         for pred_bbox, pred_label, pred_score, gt_bbox, gt_label, gt_difficult in zip(
                 *[as_numpy(x) for x in [pred_bboxes, pred_labels, pred_scores,
