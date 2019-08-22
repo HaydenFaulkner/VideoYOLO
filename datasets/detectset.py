078c74119c49f5186f1443631d7762b4fee18645
HaydenFaulkner <hay167167>
2019-08-22 16:57:47 +0930

    clean and comment

--- datasets/detectset.py
+++ datasets/detectset.py
@@ -7,24 +7,61 @@ import numpy as np
 
 
 class DetectSet(Dataset):
+    """
+    A dataset that is simply compiled of a file list
+    """
     def __init__(self, file_list):
+        """
+        Args:
+            file_list (list): a list of full paths to the image files
+        """
         super(DetectSet, self).__init__()
         self._file_list = file_list
 
     def __len__(self):
+        """
+        The length of the dataset
+
+        Returns:
+            int: the number of samples in the dataset
+        """
         return len(self._file_list)
 
     def __getitem__(self, idx):
-        img_path = self._file_list[idx]
-        img = mx.image.imread(img_path, 1)
-        label = self._load_label(idx)
+        """
+        Get a sample from the dataset
+
+        Args:
+            idx (int): index of the sample in the dataset
+
+        Returns:
+            mxnet.NDArray: input image/volume
+            numpy.ndarray: label
+            int: idx (if inference=True)
+        """
+        img_path = self.sample_path(idx)  # get the image path for the sample
+        img = mx.image.imread(img_path, 1)  # load the image using mxnet's imread()
+        label = self.load_label()  # load the label
         return img, label, idx
 
     def sample_path(self, idx):
+        """
+        Get a path to an image file provided the sample index
+
+        Args:
+            idx (int): the sample index, a number from 0 to len(dataset)
+
+        Returns:
+            str: path to the image file
+        """
         return self._file_list[idx]
 
-    def _load_label(self, idx):
-        img_path = self._file_list[idx]
-        label = list()
-        label.append([-1, -1, -1, -1, -1])
-        return np.array(label)
+    @staticmethod
+    def load_label():
+        """
+        Get the label for a sample, in this case there are none so just give -1's
+
+        Returns:
+            numpy.ndarray: shape (1,5) full of -1's
+        """
+        return np.ones((1, 5))*-1
