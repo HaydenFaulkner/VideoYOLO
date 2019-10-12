41d9e0d6f0844771c5588b9d4d188ef790954ee0
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2019-10-12 23:21:47 +1030

    debugged flownet and cleaned up a bit

--- models/definitions/flownet/flownetS.py
+++ models/definitions/flownet/flownetS.py
@@ -4,139 +4,134 @@ Philipp Fischer et al.
 
 from https://arxiv.org/pdf/1504.06852.pdf
 """
+
+import mxnet as mx
+from mxnet import gluon, autograd
 from mxnet.gluon.block import HybridBlock
 from mxnet.gluon import nn
 
+from utils import convert_weights
+
 class FlowNetS(HybridBlock):
     """
-    FlowNet S
+    FlowNet S without batch norm
     """
-    def __init__(self, prefix='flownetS', **kwargs):
+    def __init__(self, prefix='flownetS', div_flow=20, **kwargs):
         super(FlowNetS, self).__init__(**kwargs)
+        self.div_flow = div_flow
         with self.name_scope():
-            self.down_1 = nn.HybridSequential(prefix=prefix+'_down_1')
-            self.down_1.add(nn.AvgPool2D(pool_size=2, strides=2, padding=0, ceil_mode=True, prefix='resize_data'))
-            self.down_1.add(nn.Conv2D(channels=64, kernel_size=7, strides=2, padding=3, prefix='flow_conv1'))
-            self.down_1.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU1'))
-            self.down_1.add(nn.Conv2D(channels=128, kernel_size=5, strides=2, padding=2, prefix='conv2'))
-            self.down_1.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU2'))
+            self.conv1 = nn.HybridSequential(prefix=prefix+'_conv_1.')
+            self.conv1.add(nn.Conv2D(channels=64, kernel_size=7, strides=2, padding=3, prefix='conv1.0.'))
+            self.conv1.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU1.'))
 
-            self.down_2 = nn.HybridSequential(prefix=prefix+'_down_2')
-            self.down_2.add(nn.Conv2D(channels=256, kernel_size=5, strides=2, padding=2, prefix='conv3'))
-            self.down_2.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU3'))
-            self.down_2.add(nn.Conv2D(channels=256, kernel_size=3, strides=1, padding=1, prefix='conv3_1'))
-            self.down_2.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU4'))
+            self.conv2 = nn.HybridSequential(prefix=prefix + '_conv_2.')
+            self.conv2.add(nn.Conv2D(channels=128, kernel_size=5, strides=2, padding=2, prefix='conv2.0.'))
+            self.conv2.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU2.'))
 
-            self.down_3 = nn.HybridSequential(prefix=prefix+'_down_3')
-            self.down_3.add(nn.Conv2D(channels=512, kernel_size=3, strides=2, padding=1, prefix='conv4'))
-            self.down_3.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU5'))
-            self.down_3.add(nn.Conv2D(channels=512, kernel_size=3, strides=1, padding=1, prefix='conv4_1'))
-            self.down_3.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU6'))
+            self.conv3 = nn.HybridSequential(prefix=prefix+'_conv_3.')
+            self.conv3.add(nn.Conv2D(channels=256, kernel_size=5, strides=2, padding=2, prefix='conv3.0.'))
+            self.conv3.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU3.'))
+            self.conv3.add(nn.Conv2D(channels=256, kernel_size=3, strides=1, padding=1, prefix='conv3_1.0.'))
+            self.conv3.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU4.'))
 
-            self.down_4 = nn.HybridSequential(prefix=prefix+'_down_4')
-            self.down_4.add(nn.Conv2D(channels=512, kernel_size=3, strides=2, padding=1, prefix='conv5'))
-            self.down_4.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU7'))
-            self.down_4.add(nn.Conv2D(channels=512, kernel_size=3, strides=1, padding=1, prefix='conv5_1'))
-            self.down_4.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU8'))
+            self.conv4 = nn.HybridSequential(prefix=prefix+'_conv_4.')
+            self.conv4.add(nn.Conv2D(channels=512, kernel_size=3, strides=2, padding=1, prefix='conv4.0.'))
+            self.conv4.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU5.'))
+            self.conv4.add(nn.Conv2D(channels=512, kernel_size=3, strides=1, padding=1, prefix='conv4_1.0.'))
+            self.conv4.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU6.'))
 
-            self.down_5 = nn.HybridSequential(prefix=prefix+'_down_5')
-            self.down_5.add(nn.Conv2D(channels=1024, kernel_size=3, strides=2, padding=1, prefix='conv6'))
-            self.down_5.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU9'))
-            self.down_5.add(nn.Conv2D(channels=1024, kernel_size=3, strides=1, padding=1, prefix='conv6_1'))
-            self.down_5.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU10'))
+            self.conv5 = nn.HybridSequential(prefix=prefix+'_conv_5.')
+            self.conv5.add(nn.Conv2D(channels=512, kernel_size=3, strides=2, padding=1, prefix='conv5.0.'))
+            self.conv5.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU7.'))
+            self.conv5.add(nn.Conv2D(channels=512, kernel_size=3, strides=1, padding=1, prefix='conv5_1.0.'))
+            self.conv5.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU8.'))
 
-            self.conv1 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='Convolution1')
-            self.dec5 = nn.Conv2DTranspose(channels=512, kernel_size=4, strides=2, padding=0, prefix='deconv5')
-            self.relu11 = nn.LeakyReLU(alpha=0.1, prefix='ReLU11')
-            self.up6to5 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=0, prefix='upsample_flow6to5')
+            self.conv6 = nn.HybridSequential(prefix=prefix+'_conv_6.')
+            self.conv6.add(nn.Conv2D(channels=1024, kernel_size=3, strides=2, padding=1, prefix='conv6.0.'))
+            self.conv6.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU9.'))
+            self.conv6.add(nn.Conv2D(channels=1024, kernel_size=3, strides=1, padding=1, prefix='conv6_1.0.'))
+            self.conv6.add(nn.LeakyReLU(alpha=0.1, prefix='ReLU10.'))
 
-            self.conv2 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='Convolution2')
-            self.dec4 = nn.Conv2DTranspose(channels=256, kernel_size=4, strides=2, padding=0, prefix='deconv4')
-            self.relu12 = nn.LeakyReLU(alpha=0.1, prefix='ReLU12')
-            self.up5to4 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=0, prefix='upsample_flow5to4')
+            self.predict_flow6 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='predict_flow6.')
+            self.deconv5 = nn.Conv2DTranspose(channels=512, kernel_size=4, strides=2, padding=1, prefix='deconv5.0.')
+            self.relu11 = nn.LeakyReLU(alpha=0.1, prefix='ReLU11.')
+            self.upsampled_flow6_to_5 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=1,
+                                                           prefix='upsampled_flow6_to_5.', use_bias=False)
 
-            self.conv3 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='Convolution3')
-            self.dec3 = nn.Conv2DTranspose(channels=128, kernel_size=4, strides=2, padding=0, prefix='deconv3')
-            self.relu13 = nn.LeakyReLU(alpha=0.1, prefix='ReLU13')
-            self.up4to3 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=0, prefix='upsample_flow4to3')
+            self.predict_flow5 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='predict_flow5.')
+            self.deconv4 = nn.Conv2DTranspose(channels=256, kernel_size=4, strides=2, padding=1, prefix='deconv4.0.')
+            self.relu12 = nn.LeakyReLU(alpha=0.1, prefix='ReLU12.')
+            self.upsampled_flow5_to_4 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=1,
+                                                           prefix='upsampled_flow5_to_4.', use_bias=False)
 
-            self.conv4 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='Convolution4')
-            self.dec2 = nn.Conv2DTranspose(channels=64, kernel_size=4, strides=2, padding=0, prefix='deconv2')
-            self.relu14 = nn.LeakyReLU(alpha=0.1, prefix='ReLU14')
-            self.up3to2 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=0, prefix='upsample_flow3to2')
+            self.predict_flow4 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='predict_flow4.')
+            self.deconv3 = nn.Conv2DTranspose(channels=128, kernel_size=4, strides=2, padding=1, prefix='deconv3.0.')
+            self.relu13 = nn.LeakyReLU(alpha=0.1, prefix='ReLU13.')
+            self.upsampled_flow4_to_3 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=1,
+                                                           prefix='upsampled_flow4_to_3.', use_bias=False)
 
-            # self.pool = nn.AvgPool2D(pool_size=2, strides=2, padding=0, ceil_mode=True, prefix='resize_concat5')
-            self.conv5 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='Convolution5')
+            self.predict_flow3 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='predict_flow3.')
+            self.deconv2 = nn.Conv2DTranspose(channels=64, kernel_size=4, strides=2, padding=1, prefix='deconv2.0.')
+            self.relu14 = nn.LeakyReLU(alpha=0.1, prefix='ReLU14.')
+            self.upsampled_flow3_to_2 = nn.Conv2DTranspose(channels=2, kernel_size=4, strides=2, padding=1,
+                                                           prefix='upsampled_flow3_to_2.', use_bias=False)
+
+            self.predict_flow2 = nn.Conv2D(channels=2, kernel_size=3, strides=1, padding=1, prefix='predict_flow2.')
+
 
     def hybrid_forward(self, F, x):
 
-        x_1 = self.down_1(x)
-        x_2 = self.down_2(x_1)
-        x_3 = self.down_3(x_2)
-        x_4 = self.down_4(x_3)
-        x_5 = self.down_5(x_4)
+        x = F.reshape(x, shape=(0,-3,-2))  # concat the two imgs on the channels dim b,i,c,h,w -> b,i*c,h,w
 
-        x_6f = self.conv1(x_5)
+        out_conv1 = self.conv1(x)
+        out_conv2 = self.conv2(out_conv1)
+        out_conv3 = self.conv3(out_conv2)
+        out_conv4 = self.conv4(out_conv3)
+        out_conv5 = self.conv5(out_conv4)
+        out_conv6 = self.conv6(out_conv5)
 
-        x_6a = self.dec5(x_5)
-        x_6a = x_6a[:,:,1:-1,1:-1]  # crop is there a better way to do it? crop to wh of x_4
-        x_6a = self.relu11(x_6a)
+        flow6 = self.predict_flow6(out_conv6)
+        flow6_up = self.upsampled_flow6_to_5(flow6)
+        out_deconv5 = self.relu11(self.deconv5(out_conv6))
 
-        x_6b = self.up6to5(x_6f)
-        x_6b = x_6b[:,:,1:-1,1:-1] # crop to wh of x_4
+        concat5 = F.concat(out_conv5, out_deconv5, flow6_up)
+        flow5 = self.predict_flow5(concat5)
+        flow5_up = self.upsampled_flow5_to_4(flow5)
+        out_deconv4 = self.relu12(self.deconv4(concat5))
 
-        x_6 = F.concat(x_4, x_6a, x_6b)
+        concat4 = F.concat(out_conv4, out_deconv4, flow5_up)
+        flow4 = self.predict_flow4(concat4)
+        flow4_up = self.upsampled_flow4_to_3(flow4)
+        out_deconv3 = self.relu13(self.deconv3(concat4))
 
-        x_7f = self.conv2(x_6)
+        concat3 = F.concat(out_conv3, out_deconv3, flow4_up)
+        flow3 = self.predict_flow3(concat3)
+        flow3_up = self.upsampled_flow3_to_2(flow3)
+        out_deconv2 = self.relu14(self.deconv2(concat3))
 
-        x_7a = self.dec4(x_6)
-        x_7a = x_7a[:,:,1:-1,1:-1]  # crop to wh of x_3
-        x_7a = self.relu12(x_7a)
+        concat2 = F.concat(out_conv2, out_deconv2, flow3_up)
+        flow2 = self.predict_flow2(concat2)
 
-        x_7b = self.up5to4(x_7f)
-        x_7b = x_7b[:,:,1:-1,1:-1] # crop to wh of x_3
-
-        x_7 = F.concat(x_3, x_7a, x_7b)
-
-        x_8f = self.conv3(x_7)
-
-        x_8a = self.dec3(x_7)
-        x_8a = x_8a[:,:,1:-1,1:-1]  # crop to wh of x_2
-        x_8a = self.relu13(x_8a)
-
-        x_8b = self.up4to3(x_8f)
-        x_8b = x_8b[:,:,1:-1,1:-1] # crop to wh of x_2
-
-        x_8 = F.concat(x_2, x_8a, x_8b)
-
-        x_9f = self.conv4(x_8)
-
-        x_9a = self.dec2(x_8)
-        x_9a = x_9a[:,:,1:-1,1:-1]  # crop to wh of x_1
-        x_9a = self.relu14(x_9a)
-
-        x_9b = self.up3to2(x_9f)
-        x_9b = x_9b[:,:,1:-1,1:-1] # crop to wh of x_1
-
-        x_9 = F.concat(x_1, x_9a, x_9b)
-
-        x_10f = self.conv5(x_9)
-
-        # x = self.pool(x_9)
         if autograd.is_training():
-            return x_10f, x_9f, x_8f, x_7f, x_6f
-        return x_10f
+            return flow2, flow3, flow4, flow5, flow6
+
+        return flow2 # do upsampling in numpy outside as F.UpSampling() bilinear is very broken
 
 
 if __name__ == '__main__':
-    # just for debugging
-    import mxnet as mx
-    from mxnet import gluon, autograd
+
+    # contexts
+    gpus = [0]
+    ctx = [mx.gpu(int(i)) for i in gpus]
+    ctx = ctx if ctx else [mx.cpu()]
 
     net = FlowNetS()
     net.initialize()
 
-    # mse_loss = gluon.loss.L2Loss()
-    # with autograd.record():
-    out = net.summary(mx.nd.ones((1, 6, 384, 512)))
+    out = net.summary(mx.nd.random_normal(shape=(1, 2, 3, 384, 512)))
+
+    # convert_weights(net, load_path='/home/hayden/Downloads/FlowNet2-S_checkpoint.pth')
+    # net.load_parameters('/home/hayden/Downloads/FlowNet2-S_checkpoint.params')
+
+    net.collect_params().reset_ctx(ctx)
     print()
---
+++ models/definitions/flownet/inference.py
@@ -0,0 +1,157 @@
+import cv2
+import mxnet as mx
+import numpy as np
+from scipy.misc import imresize
+from tqdm import tqdm
+
+from flownetS import FlowNetS
+from utils import flow_to_image, crop, normalise
+
+def process_two_images(model, imgs, ctx=None):
+    """
+    Process two images into one flow image
+    Args:
+        model: The model to use
+        imgs: a list of 2 images
+        ctx: the model ctx
+
+    Returns:
+
+    """
+    if len(imgs) != 2:
+        return None
+    if isinstance(imgs[0], str):
+        if os.path.exists(imgs[0]):
+            imgs[0] = cv2.cvtColor(cv2.imread(files[i]), cv2.COLOR_BGR2RGB)
+        else:
+            return None
+    if isinstance(imgs[1], str):
+        if os.path.exists(imgs[1]):
+            imgs[1] = cv2.cvtColor(cv2.imread(files[i]), cv2.COLOR_BGR2RGB)
+        else:
+            return None
+
+    imgs = crop(imgs)
+    imgs = np.array(imgs)
+    imgs = np.moveaxis(imgs, -1, 1)
+    imgs = normalise(imgs)
+
+    imgs = mx.nd.array(imgs, ctx=ctx)
+    imgs = mx.nd.expand_dims(imgs, 0)  # add batch axis
+
+    flow = model(imgs)  # run the model
+
+    flow = flow.asnumpy()
+    flow = flow.squeeze()
+    flow = flow.transpose(1, 2, 0)
+    img = flow_to_image(flow)
+    img = imresize(img, 4.0)  # doing the bilinear interpolation on the img, NOT flow cause was too hard :'(
+
+    return img, flow
+
+
+def process_imagedir(model, input_dir, output_dir=None, ctx=None):
+    """
+    Process a directory of images
+
+    Args:
+        model:
+        input_dir:
+        output_dir:
+        ctx:
+
+    Returns:
+
+    """
+
+    files = []
+    for ext in [".jpg", ".png", ".jpeg", ".JPG", ".PNG", ".JPEG"]:
+        files = glob.glob(input_dir + "/**/*" + ext, recursive=True)
+        if len(files) > 0:
+            break
+
+    if not len(files) > 0:
+        print("Couldn't find any files in {}".format(input_dir))
+        return None
+
+    files.sort()
+
+    for i in tqdm(range(len(files) - 1), desc='Calculating Flow'):
+        img, flow = process_two_images(model, files[i:i+2], ctx)
+        dir, file = os.path.split(files[i])
+        if output_dir is None:
+            output_dir = os.path.join(dir, 'flow')
+        os.makedirs(output_dir, exists_ok=True)
+        cv2.imwrite(os.path.join(output_dir, file), cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    return output_dir
+
+
+def process_video(model, input_path, output_path=None, ctx=None):
+    """
+    Process a video into a flow video
+
+    Args:
+        model:
+        input_path:
+        output_path:
+        ctx:
+
+    Returns:
+
+    """
+    capture = cv2.VideoCapture(input_path)
+    frames = []
+    while_safety = 0
+    while len(frames) < 200:# int(capture.get(cv2.CAP_PROP_FRAME_COUNT))-1:
+        _, image = capture.read()  # read an image from the capture
+
+        if while_safety > 500:  # break the while if our safety maxs out at 500
+            break
+
+        if image is None:
+            while_safety += 1
+            continue
+
+        while_safety = 0  # reset the safety count
+        frames.append(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    capture.release()
+
+    if len(frames) < 2:
+        return None
+
+    if output_path is None:
+        output_path = input_path[:-4] + '_flow.mp4'
+
+    cropped_frames = crop(frames)
+    h, w, _= cropped_frames[0].shape
+    video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 25, (w, h))
+
+    for i in tqdm(range(len(frames)-1), desc='Calculating Flow'):
+        mx.nd.waitall()
+        img, flow = process_two_images(model, frames[i:i+2], ctx)
+        video.write(img)
+
+    video.release()  # release the video
+
+    return output_path
+
+if __name__ == '__main__':
+
+    # contexts
+    # gpus = [0]
+    # ctx = [mx.gpu(int(i)) for i in gpus]
+    # ctx = ctx if ctx else [mx.cpu()]
+
+    ctx = mx.gpu(0)
+
+    net = FlowNetS()
+    net.initialize()
+
+    net.load_parameters('/home/hayden/Downloads/FlowNet2-S_checkpoint.params')
+    net.collect_params().reset_ctx(ctx)
+    net.hybridize()
+
+    input_path = "/home/hayden/Downloads/test.mp4"
+    process_video(net, input_path, ctx=ctx)
+    print("DONE")
