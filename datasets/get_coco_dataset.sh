5ef06863eea4651e43a1b76a1bac0274297673c0
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2019-11-20 14:41:04 +1030

    update get coco set

--- datasets/get_coco_dataset.sh
+++ datasets/get_coco_dataset.sh
@@ -9,6 +9,8 @@ git clone https://github.com/pdollar/coco
 mv coco cocoapi
 
 # Download and Unzip Images 2014 then 2017
+mkdir images
+cd images
 wget -c http://images.cocodataset.org/zips/train2014.zip
 wget -c http://images.cocodataset.org/zips/val2014.zip
 wget -c http://images.cocodataset.org/zips/test2014.zip
@@ -25,6 +27,8 @@ unzip -q train2017.zip
 unzip -q val2017.zip
 unzip -q test2017.zip
 
+cd ..
+
 
 # Download COCO Metadata
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2014.zip
@@ -34,16 +38,5 @@ wget -c http://images.cocodataset.org/annotations/image_info_test2017.zip
 unzip -q annotations_trainval2014.zip
 unzip -q annotations_trainval2017.zip
 unzip -q image_info_test2017.zip
-
-
-#wget -c https://pjreddie.com/media/files/coco/labels.tgz
-#tar xzf labels.tgz
-
-
-# Set Up Image Lists
-#paste <(awk "{print \"$PWD\"}" <5k.part) 5k.part | tr -d '\t' > 5k_abs.txt
-#paste <(awk "{print \"$PWD\"}" <trainvalno5k.part) trainvalno5k.part | tr -d '\t' > trainvalno5k_abs.txt
-#paste <(awk "{print \"$PWD\"}" <val2017.txt) val2017.txt | tr -d '\t' > val2017_abs.txt
-#paste <(awk "{print \"$PWD\"}" <train2017.txt) train2017.txt | tr -d '\t' > train2017_abs.txt
 
 cd ..
